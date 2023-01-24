e9084d0d6842d392878669e347293771c8840264
Mujammil-ios <mujammilpinjari68507@gmail.com>
2023-01-24 20:16:34 +0500

    update

--- app/src/main/java/com/example/collageassignmentrecyclerview/NewsModel.kt
+++ app/src/main/java/com/example/collageassignmentrecyclerview/NewsModel.kt
@@ -1,5 +1,7 @@
 package com.example.collageassignmentrecyclerview
 data class NewsModel(
+//    recyclerview Model
+
     var title: String = "",
     var description: String = "",
     var date: String = "",
